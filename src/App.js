f05c5afd5933059625142e8ae441acdee3428574
andreevanatasha <tinskimmings@gmail.com>
2017-09-06 23:20:58 +0300

    add App component and basic styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Textfit } from 'react-textfit';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+            placeholder: 'Type anything',
+            background: '#f28ec2',
+            fullscreen: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.clearInput = this.clearInput.bind(this);
+        this.closeFullScreen = this.closeFullScreen.bind(this);
+        this.openFullScreen = this.openFullScreen.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({text: e.target.value});
+    }
+
+    clearInput() {
+        this.setState({text: ''});
+    }
+
+    openFullScreen() {
+        this.setState({fullscreen: true});
+    }
+
+    closeFullScreen() {
+        this.setState({fullscreen: false});
+    }
+
+
+    render () {
+        let styles = {
+                width: screen.availWidth,
+                height: screen.availHeight,
+                background: this.state.background
+        };
+
+        if (this.state.fullscreen === false) { 
+            return (
+                <div className="main" style={styles}> 
+                    <div className="header">
+                        <button className="btn clear" onClick={this.clearInput}>Clear</button>
+                        <button className="btn run" onClick={this.openFullScreen}>Run</button>
+                    </div>
+                    <textarea className="input" style={{background: this.state.background}} onChange={this.handleChange} placeholder={this.state.placeholder} type="text" value={this.state.text} />
+                </div>
+                )
+        } else {
+            return (
+            <div className='rotate' style={styles}>
+                <button className="btn" onClick={this.closeFullScreen}>Close</button>
+                <Textfit>
+                  {this.state.text}
+                </Textfit>
+            </div>
+            )
+        };
   }
 }
 
