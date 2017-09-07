5d4a000aca1bd1c80a9e002b132eeb914c345c14
andreevanatasha <tinskimmings@gmail.com>
2017-09-08 02:04:46 +0300

    Add Swipe to change background feature

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { Textfit } from 'react-textfit';
+import Swipe from 'react-easy-swipe';
 import './App.css';
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             text: '',
-            placeholder: 'Type anything',
-            background: '#f28ec2',
-            fullscreen: false
+            placeholder: 'Type anything\nSwipe to change background',
+            background_id: 0,
+            fullscreen: false,
+            backgrounds: [
+                '#f28ec2',
+                '#49c086',
+                '#9673e1',
+                '#ecb755'
+            ],
+            backgrounds_number: 3
         };
+
         this.handleChange = this.handleChange.bind(this);
         this.clearInput = this.clearInput.bind(this);
         this.closeFullScreen = this.closeFullScreen.bind(this);
         this.openFullScreen = this.openFullScreen.bind(this);
+        this.onSwipeLeft = this.onSwipeLeft.bind(this);
+        this.onSwipeRight = this.onSwipeRight.bind(this);
     }
 
     handleChange(e) {
@@ -34,18 +46,39 @@ class App extends React.Component {
         this.setState({fullscreen: false});
     }
 
+    onSwipeLeft() {
+        let current_background = this.state.background_id;
+
+        if (current_background === 0) {
+            this.setState({background_id: this.state.backgrounds_number});
+        } else {
+            current_background -= 1;
+            this.setState({background_id: current_background});
+        };
+    }
+
+    onSwipeRight() {
+        let current_background = this.state.background_id;
+
+        if (current_background === this.state.backgrounds_number) {
+            this.setState({background_id: 0});
+        } else {
+            current_background += 1;
+            this.setState({background_id: current_background});
+        };
+    }
 
     render () {
-
-        let rotate, screenWidth, screenHeight;
+        let rotate, screenWidth, screenHeight, background_color;
         screenWidth = window.innerWidth
         screenHeight = window.innerHeight;
+        background_color = this.state.backgrounds[this.state.background_id];
 
         if (screenWidth < screenHeight) {
             rotate = {
                 width: screenHeight,
                 height: screenWidth,
-                background: this.state.background,
+                background: background_color,
                 transform: "rotate(90deg)",
                 transformOrigin: "bottom left",
                 top: "-100vw"
@@ -54,34 +87,42 @@ class App extends React.Component {
              rotate = {
                 width: screenWidth,
                 height: screenHeight,
-                background: this.state.background
+                background: background_color
             };
         };
 
         if (this.state.fullscreen === false) { 
             return (
-                <div className="main" style={{background: this.state.background}} > 
-                    <div className="header" style={{background: this.state.background}}>
-                        <button className="btn clear" onClick={this.clearInput}>Clear</button>
-                        <button className="btn run" onClick={this.openFullScreen} disabled={!this.state.text}>Run</button>
+                <Swipe
+                    onSwipeLeft={this.onSwipeLeft}
+                    onSwipeRight={this.onSwipeRight} >
+                    <div className="main" style={{background: background_color}} > 
+                        <div className="header" style={{background: background_color}}>
+                            <button className="btn clear" onClick={this.clearInput}>Clear</button>
+                            <button className="btn run" onClick={this.openFullScreen} disabled={!this.state.text}>Run</button>
+                        </div>
+                        <textarea 
+                            className="input" 
+                            style={{background: background_color}} 
+                            onChange={this.handleChange} 
+                            placeholder={this.state.placeholder} 
+                            type="text" 
+                            value={this.state.text} />
                     </div>
-                    <textarea 
-                        className="input" 
-                        style={{background: this.state.background}} 
-                        onChange={this.handleChange} 
-                        placeholder={this.state.placeholder} 
-                        type="text" 
-                        value={this.state.text} />
-                </div>
+                </Swipe>
                 )
         } else {
             return (
-            <div className='fullscreen' style={rotate}>
-                <button className="btn close" onClick={this.closeFullScreen}>Close</button>
-                <Textfit max={500} style={{height: '100%'}}>
-                  {this.state.text}
-                </Textfit>
-            </div>
+            <Swipe
+                onSwipeLeft={this.onSwipeLeft}
+                onSwipeRight={this.onSwipeRight} >
+                <div className='fullscreen' style={rotate}>
+                    <button className="btn close" onClick={this.closeFullScreen}>Close</button>
+                    <Textfit max={500} style={{height: '100%'}}>
+                      {this.state.text}
+                    </Textfit>
+                </div>
+            </Swipe>
             )
         };
     }
