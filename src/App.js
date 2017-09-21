742c2796d4cdb21798bdb0921216125c5fc0dec7
andreevanatasha <tinskimmings@gmail.com>
2017-09-21 20:02:13 +0300

    Allow mouse events and fix Textfit align

--- src/App.js
+++ src/App.js
@@ -110,7 +110,8 @@ class App extends React.Component {
             return (
                 <Swipe
                     onSwipeLeft={this.onSwipeLeft}
-                    onSwipeRight={this.onSwipeRight} >
+                    onSwipeRight={this.onSwipeRight} 
+                    allowMouseEvents={true} >
                     <div className="main" style={{backgroundColor: background_color}} > 
                         <div className="header" style={{background: background_color}}>
                             <Button name='clear' disable={this.state.text} action={this.clearInput} />
@@ -130,14 +131,15 @@ class App extends React.Component {
             return (
                <Swipe
                     onSwipeLeft={this.onSwipeLeft}
-                    onSwipeRight={this.onSwipeRight} >
+                    onSwipeRight={this.onSwipeRight} 
+                    allowMouseEvents={true} >
                     <div className='fullscreen' style={rotate}>
                     {/*height: 20% on the header ain't a neato solution but since we won't have rotation on web and won't have landscape mode on mobile it'll do*/}
-                        <div className="header" style={{background: background_color, flexDirection: 'row-reverse', height: '20%'}}>
+                        <div className="header" style={{background: background_color, flexDirection: 'row-reverse', height: '15%'}}>
                             <Button name='close' disable={this.state.text} action={this.closeFullScreen} />
                         </div>
-                        <div style={{display: 'flex', flex: '1', justifyContent: 'center', alignContent: 'center'}}>
-	                        <Textfit max={500} style={{height: '100%', width: '100%', textAlign: 'center', lineHeight: 1, alignSelf: 'center'}}>
+                        <div style={{display: 'table', width: '98%', height: '85%', margin: 'auto'}}>
+	                        <Textfit max={500} style={{display: 'table-cell', height: '100%', width: '100%', textAlign: 'center', lineHeight: 1, verticalAlign: 'middle'}}>
 	                          {this.state.text}
 	                        </Textfit>
                         </div>
